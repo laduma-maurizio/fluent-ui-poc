bcc619548574de21292fe491568ff61e7029effd
Maurizio Liberato <maurizioliberato@Maurizios-MacBook-Pro-4.local>
2021-11-30 13:32:24 +0000

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,162 @@
-import React from 'react';
-import { Stack, Text, Link, FontWeights, IStackTokens, IStackStyles, ITextStyles } from '@fluentui/react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  ContextualMenu,
+  IContextualMenuProps,
+  Checkbox,
+  DefaultButton,
+  IconButton,
+  IIconProps,
+  Stack,
+  IStackStyles,
+  IStackTokens,
+  Panel,
+  PanelType,
+  ThemeProvider,
+  initializeIcons,
+  createTheme,
+  DefaultEffects,
+} from "@fluentui/react";
+import { useBoolean } from "@fluentui/react-hooks";
+import styled from "styled-components";
+import { ladumaTheme } from "./theme";
 
-const boldStyle: Partial<ITextStyles> = { root: { fontWeight: FontWeights.semibold } };
-const stackTokens: IStackTokens = { childrenGap: 15 };
-const stackStyles: Partial<IStackStyles> = {
-  root: {
-    width: '960px',
-    margin: '0 auto',
-    textAlign: 'center',
-    color: '#605e5c',
-  },
+// Initialize icons in case this example uses them
+initializeIcons();
+
+const ladTheme = createTheme(ladumaTheme);
+
+const themedLargeStackTokens: IStackTokens = {
+  childrenGap: "l1",
+  padding: "s",
 };
 
+const stackStyles: IStackStyles = {
+  root: {
+    background: ladTheme.palette.white,
+    marginBottom: "2rem",
+  },
+};
+const Card = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${ladTheme.palette.themePrimary};
+  box-shadow: ${DefaultEffects.elevation16};
+  color: ${ladTheme.palette.white};
+  height: 60px;
+  width: 60px;
+`;
+
+const navIcon: IIconProps = { iconName: "GlobalNavButton" };
+
+const Trigger = styled.div`
+  box-shadow: ${DefaultEffects.elevation8};
+  background: ${ladTheme.palette.white};
+  position: fixed;
+  left: 1rem;
+  top: 50%;
+  width: 32px;
+  height: 32px;
+  border-radius: 32px;
+  overflow: hidden;
+`;
+
+const menuProps: IContextualMenuProps = {
+  // For example: disable dismiss if shift key is held down while dismissing
+  onDismiss: (ev) => {
+    if (ev && "shiftKey" in ev) {
+      ev.preventDefault();
+    }
+  },
+  items: [
+    {
+      key: "manageDevices",
+      text: "Manage Devices",
+      iconProps: { iconName: "Devices2" },
+    },
+    {
+      key: "leaveMeeting",
+      text: "Leave Meeting",
+      iconProps: { iconName: "Leave" },
+    },
+  ],
+  directionalHintFixed: true,
+};
+
+const addIcon: IIconProps = { iconName: "SetAction" };
+
 export const App: React.FunctionComponent = () => {
+  const [isOpen, { setTrue: openPanel, setFalse: dismissPanel }] = useBoolean(false);
+
+  const _getMenu = (props: IContextualMenuProps): JSX.Element => {
+    // Customize contextual menu with menuAs
+    return <ContextualMenu {...props} />;
+  };
+
+  const _onMenuClick = (ev?: React.SyntheticEvent<any>) => {
+    console.log(ev);
+  };
+
   return (
-    <Stack horizontalAlign="center" verticalAlign="center" verticalFill styles={stackStyles} tokens={stackTokens}>
-      <img className="App-logo" src={logo} alt="logo" />
-      <Text variant="xxLarge" styles={boldStyle}>
-        Welcome to your Fluent UI app
-      </Text>
-      <Text variant="large">For a guide on how to customize this project, check out the Fluent UI documentation.</Text>
-      <Text variant="large" styles={boldStyle}>
-        Essential links
-      </Text>
-      <Stack horizontal tokens={stackTokens} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/get-started/web">Docs</Link>
-        <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">Stack Overflow</Link>
-        <Link href="https://github.com/microsoft/fluentui/">Github</Link>
-        <Link href="https://twitter.com/fluentui">Twitter</Link>
-      </Stack>
-      <Text variant="large" styles={boldStyle}>
-        Design system
-      </Text>
-      <Stack horizontal tokens={stackTokens} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/styles/web/icons">Icons</Link>
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/styles/web">Styles</Link>
-        <Link href="https://aka.ms/themedesigner">Theme designer</Link>
-      </Stack>
-    </Stack>
+    <div>
+      <ThemeProvider theme={ladTheme}>
+        <Trigger>
+          <IconButton iconProps={navIcon} title="Menu" ariaLabel="Menu" onClick={openPanel} />
+        </Trigger>
+        <Panel
+          isOpen={isOpen}
+          type={PanelType.customNear}
+          onDismiss={dismissPanel}
+          isLightDismiss
+          hasCloseButton={false}
+          customWidth="300px"
+        >
+          <Stack>
+            <h1>Meeting</h1>
+            <Stack>
+              <h3>Whiteboards</h3>
+              <Stack horizontal styles={stackStyles} tokens={themedLargeStackTokens}>
+                <Card>1</Card>
+                <Card>2</Card>
+                <Card>3</Card>
+              </Stack>
+            </Stack>
+            <Stack styles={stackStyles}>
+              <h3>Shared Content</h3>
+              <Stack horizontal styles={stackStyles} tokens={themedLargeStackTokens}>
+                <Card>1</Card>
+                <Card>2</Card>
+                <Card>3</Card>
+              </Stack>
+            </Stack>
+            {/* <Stack styles={stackStyles}>
+              <Checkbox
+                label="Unchecked checkbox"
+                onChange={() => {
+                  alert("Hello");
+                }}
+              />
+            </Stack> */}
+            <Stack>
+              <DefaultButton
+                text="Actions"
+                iconProps={addIcon}
+                menuProps={menuProps}
+                // Optional callback to customize menu rendering
+                menuAs={_getMenu}
+                // Optional callback to do other actions (besides opening the menu) on click
+                onMenuClick={_onMenuClick}
+                // By default, the ContextualMenu is re-created each time it's shown and destroyed when closed.
+                // Uncomment the next line to hide the ContextualMenu but persist it in the DOM instead.
+                // persistMenu={true}
+                allowDisabledFocus
+                // disabled={disabled}
+                // checked={checked}
+              />
+            </Stack>
+          </Stack>
+        </Panel>
+      </ThemeProvider>
+    </div>
   );
 };
---
+++ src/theme.ts
@@ -0,0 +1,27 @@
+export const ladumaTheme = {
+   
+      palette: {
+        themePrimary: '#ee722e',
+        themeLighterAlt: '#090402',
+        themeLighter: '#261207',
+        themeLight: '#47220e',
+        themeTertiary: '#8e431b',
+        themeSecondary: '#d16328',
+        themeDarkAlt: '#ef7e41',
+        themeDark: '#f1915d',
+        themeDarker: '#f5ad86',
+        neutralLighterAlt: '#223046',
+        neutralLighter: '#28374d',
+        neutralLight: '#314159',
+        neutralQuaternaryAlt: '#374861',
+        neutralQuaternary: '#3c4d67',
+        neutralTertiaryAlt: '#546681',
+        neutralTertiary: '#c8c8c8',
+        neutralSecondary: '#d0d0d0',
+        neutralPrimaryAlt: '#dadada',
+        neutralPrimary: '#ffffff',
+        neutralDark: '#f4f4f4',
+        black: '#f8f8f8',
+        white: '#1d2a3e',
+      }
+};
